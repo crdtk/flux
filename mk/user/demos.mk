0e100b79035c2dec87d33328b3a5ff5dac4bc465
Conrado M. Radtke <conradoradtke@gmail.com>
2026-06-14 21:52:36 +0200

    Add Python 3.12 support and clean up demo environment setup

--- mk/user/demos.mk
+++ mk/user/demos.mk
@@ -1,11 +1,9 @@
-.PHONY: llms-from-scratch
-
 LLMS_VENV := demos/LLMs-from-scratch/venv
 
-llms-from-scratch: $(LLMS_VENV)/bin/jupyter
+USER_FILES += $(LLMS_VENV)/bin/jupyter
 
 $(LLMS_VENV)/bin/jupyter: demos/LLMs-from-scratch/requirements.txt
-	python3 -m venv $(LLMS_VENV)
+	python3.12 -m venv $(LLMS_VENV)
 	$(LLMS_VENV)/bin/pip install -r $<
 	$(LLMS_VENV)/bin/python -m ipykernel install --user --name llms-from-scratch --display-name "LLMs-from-scratch"
 	@echo ">>> LLMs-from-scratch deps installed"
